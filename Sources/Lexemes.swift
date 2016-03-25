5f8234152a37142f1277f7c03cfeb72672c9a929
Oleg Dreyman <dreymonde@me.com>
2016-03-25 17:46:08 +0200

    Lexeme -> IDR

--- Sources/Lexemes.swift
+++ Sources/Lexemes.swift
@@ -25,10 +25,24 @@ extension ForeignLexeme: InterchangeDataRepresentable {
 	public var interchangeData: InterchangeData {
 		let data: InterchangeData = [
 			"lemma": lemma.interchangeData,
-			"forms": InterchangeData.from(forms.map({ $0.interchangeData }))
+			"forms": InterchangeData.from(forms.map({ $0.interchangeData })),
 			"origin": origin.interchangeData,
 			"meaning": InterchangeData.from(meaning),
 			"permissibility": InterchangeData.from(permissibility.rawValue)
 		]
+		return data
+	}
+	public init?(interchangeData: InterchangeData) {
+		guard let lemma = interchangeData["lemma"].flatMap(Morpheme.init),
+			forms = interchangeData["forms"]?.array?.flatMap(Morpheme.init),
+			origin = interchangeData["origin"].flatMap(Morpheme.init),
+			meaning = interchangeData["meaning"]?.string,
+			permissibility = interchangeData["permissibility"]?.int.flatMap({ Permissibility(rawValue: $0) })
+			else { return nil }
+		self.lemma = lemma
+		self.forms = forms
+		self.origin = origin
+		self.meaning = meaning
+		self.permissibility = permissibility
 	}
 }
