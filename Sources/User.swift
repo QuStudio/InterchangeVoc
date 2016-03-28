c1dc07d4d93267222f09444ee8fdf8a3a37573e1
Oleg Dreyman <dreymonde@me.com>
2016-03-28 14:19:59 +0300

    User -> Topo

--- Sources/User.swift
+++ Sources/User.swift
@@ -1,18 +1,18 @@
 import InterchangeData
 import Vocabulaire
+import Topo
 
-extension User.Status: InterchangeDataRepresentable {
-	public var interchangeData: InterchangeData {
+extension User.Status: RawRepresentable {
+	public var rawValue: String {
 		switch self {
 		case .Regular:
-			return InterchangeData.from("regular")
+			return "regular"
 		case .BoardMember:
-			return InterchangeData.from("board-member")
+			return "board-member"
 		}
 	}
-	public init?(interchangeData: InterchangeData) {
-		guard let raw = interchangeData.string else { return nil }
-		switch raw {
+	public init?(rawValue: String) {
+		switch rawValue {
 		case "regular":
 			self = .Regular
 		case "board-member":
@@ -32,13 +32,18 @@ extension User: InterchangeDataRepresentable {
 		]
 		return data
 	}
-	public init?(interchangeData: InterchangeData) {
-		guard let id = interchangeData["id"]?.int,
-			username = interchangeData["username"]?.string,
-			status = interchangeData["status"].flatMap({ Status(interchangeData: $0) })
-			else { return nil }
-		self.id = id
-		self.username = username
-		self.status = status
+	// public init?(interchangeData: InterchangeData) {
+	// 	guard let id = interchangeData["id"]?.int,
+	// 		username = interchangeData["username"]?.string,
+	// 		status = interchangeData["status"].flatMap({ Status(interchangeData: $0) })
+	// 		else { return nil }
+	// 	self.id = id
+	// 	self.username = username
+	// 	self.status = status
+	// }
+	public init(map: Mapper) throws {
+		try id = map.from("id")
+		try username = map.from("username")
+		try status = map.from("status")
 	}
 }
