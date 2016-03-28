bc7b2e9d4efa7ee6d0f40099b30e5f079506dddc
Oleg Dreyman <dreymonde@me.com>
2016-03-28 14:23:58 +0300

    Entry -> Topo

--- Sources/Entry.swift
+++ Sources/Entry.swift
@@ -1,5 +1,6 @@
 import InterchangeData
 import Vocabulaire
+import Topo
 
 extension Entry: InterchangeDataRepresentable {
 	public var interchangeData: InterchangeData {
@@ -11,16 +12,22 @@ extension Entry: InterchangeDataRepresentable {
 		]
 		return data
 	}
-	public init?(interchangeData: InterchangeData) {
-		guard let id = interchangeData["id"]?.int,
-			foreign = interchangeData["foreign"].flatMap(ForeignLexeme.init),
-			natives = interchangeData["natives"]?.array?.flatMap(NativeLexeme.init),
-			author = interchangeData["author"].flatMap(User.init)
-			else { return nil }
-		self.id = id
-		self.foreign = foreign
-		self.natives = Set(natives)
-		self.author = author
+	// public init?(interchangeData: InterchangeData) {
+	// 	guard let id = interchangeData["id"]?.int,
+	// 		foreign = interchangeData["foreign"].flatMap(ForeignLexeme.init),
+	// 		natives = interchangeData["natives"]?.array?.flatMap(NativeLexeme.init),
+	// 		author = interchangeData["author"].flatMap(User.init)
+	// 		else { return nil }
+	// 	self.id = id
+	// 	self.foreign = foreign
+	// 	self.natives = Set(natives)
+	// 	self.author = author
+	// }
+	public init(map: Mapper) throws {
+		try id = map.from("id")
+		try foreign = map.from("foreign")
+		try natives = Set(map.fromArray("natives"))
+		author = map.optionalFrom("author")
 	}
 }
 
