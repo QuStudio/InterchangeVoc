a6794b59dff74c3eae8e0799f82776dd8624fdf7
Oleg Dreyman <dreymonde@me.com>
2016-03-29 23:36:25 +0300

    Package.swift fix for Topo (which is now under Zewo)

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "InterchangeVoc",
     dependencies: [
     	.Package(url: "https://github.com/QuStudio/Vocabulaire.git", majorVersion: 0, minor: 2),
-    	.Package(url: "https://github.com/Zewo/InterchangeData.git", majorVersion: 0, minor: 2),
-    	.Package(url: "https://github.com/dreymonde/Topo.git", majorVersion: 0, minor: 3)
+//    	.Package(url: "https://github.com/Zewo/InterchangeData.git", majorVersion: 0, minor: 2),
+    	.Package(url: "https://github.com/Zewo/InterchangeDataMapper.git", majorVersion: 0, minor: 3)
     ]
 )
